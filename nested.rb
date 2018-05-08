d44ab466dc510a6fb5821d496d73d092264bf98c
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-08 22:44:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
      }
 
      alans_new_info = "GUI"
-		 programmer_hash[:alans_kay][:known_for] = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
 		 programmer_hash
 end
 
